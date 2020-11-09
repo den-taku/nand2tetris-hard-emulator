d03e0562d78fdf48dbc24fdc2c15d2b09702d869
DenTaku <dentaku1412@gmail.com>
2020-11-09 21:31:55 +0900

    Implement HalfAdder

--- rust_impl/src/arithmetic.rs
+++ rust_impl/src/arithmetic.rs
@@ -1,8 +1,39 @@
+#![allow(dead_code, non_snake_case)]
+
 use crate::logic::*;
-use crate::logic::Bit::{I, O};
+// use crate::logic::Bit::{I, O};
+
+// a, b -> curry, sum
+pub fn HalfAdder(a: Bit, b: Bit) -> [Bit; 2] {
+    [
+        And(
+            a,
+            b
+        ),
+        Xor(
+            a,
+            b
+        )
+    ]
+}
+
+// a, b, curry -> curry, sum
+pub fn FullAdder(a: Bit, b: Bit, c: Bit) -> [Bit; 2] {
+    unimplemented!()
+}
 
 
 #[cfg(test)]
 mod tests{
-    //
+    // use crate::logic::*;
+    use crate::logic::Bit::{I, O};
+    use super::{HalfAdder};
+
+    #[test]
+    fn for_halfadder() {
+        assert_eq!(HalfAdder(O, O), [O, O]);
+        assert_eq!(HalfAdder(O, I), [O, I]);
+        assert_eq!(HalfAdder(I, O), [O, I]);
+        assert_eq!(HalfAdder(I, I), [I, O]);
+    }
 }

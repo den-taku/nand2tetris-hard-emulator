f5cf39cbc30f34a60cb60a800fee9f205f882858
DenTaku <dentaku1412@gmail.com>
2020-11-10 01:09:20 +0900

    Check ALU

--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -10,4 +10,10 @@ fn main() {
     println!("{}", Word::new([O; 16]));
     println!("{}", Word::new([I; 16]));
     println!("{}", Inc16(Word::new([I; 16])));
+    println!("{:?}", 
+        ALU(
+            Word::new([O, O, O, O, O, O, O, O, O, O, O, I, O, O, O, I]),
+            Word::new([O, O, O, O, O, O, O, O, O, O, O, O, O, O, I, I]),
+            O, O, I, I, O, O
+        ));
 }

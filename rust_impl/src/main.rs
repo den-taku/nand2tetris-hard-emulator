c65306b61cc47aaf34fe01c2eb5ed758c5094f81
DenTaku <dentaku1412@gmail.com>
2020-11-13 22:44:37 +0900

    Implement RAM16K

--- rust_impl/src/main.rs
+++ rust_impl/src/main.rs
@@ -18,6 +18,32 @@ fn main() {
             Word::new([O, O, O, O, O, O, O, O, O, O, O, O, O, O, I, I]),
             O, O, I, I, O, O
         ));
+    let mut ram8 = RAM8::new();
+    let mut clock = Clock::new();
+    let word_i = Word::new([I, O, I, O, I, I, O, O, O, I, O, I, O, O, I, I]);
+    let word_o = Word::new([O, I, O, I, O, O, I, I, I, O, I, O, I, I, O, O]);
+    let word_0 = Word::new([O; 16]);
+    // input as past: word_0, new: word_i in registers
+    ram8.input(&clock, word_i, [O, I, O], I);
+    // output past in register
+    assert_eq!(ram8.output(&clock, [O, I, O]), word_0);
+
+    println!("");
+    println!("{:?}", ram8);
+    println!("");
+
+    // Tock
+    clock.next();
+
+    // nothing happened
+    ram8.input(&clock, word_o, [O, I, O], O);
+    // output new
+    assert_eq!(ram8.output(&clock, [O, I, O]), word_i);
+
+    println!("{:?}", ram8);
+
+    // Tick
+    clock.next();
     
     // let mut clock = Clock::new();
     // loop {

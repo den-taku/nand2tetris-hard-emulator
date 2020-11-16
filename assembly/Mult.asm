4ead1beb3b93a6795cd5f317a6f6844b48645d4d
DenTaku <dentaku1412@gmail.com>
2020-11-17 00:00:32 +0900

    Write Mult.asm

--- assembly/Mult.asm
+++ assembly/Mult.asm
@@ -1,1 +1,21 @@
 // Mult.asm
+// Caluculate R0 * R1 -> R2
+// R0 >= 0, R1 >= 0, R0 * Rq < 32768
+
+@R2
+M=0
+
+(LOOP)
+    @R0
+    D=M
+    @R2
+    M=M+D // R2 += R0
+
+    @R1
+    MD=M-1 // R1 -= 1
+
+    @LOOP
+    D;JGT
+(END)
+    @END
+    0;JMP
